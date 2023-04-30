f94c5026cdbfd68a3cb283f4462e28d61d5c4d47
Guilherme Andrade <guilhermeandrade.profissional@outlook.com>
2023-04-30 15:16:35 -0300

    fix: Correção da Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    protected $guarded = ['id'];
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +45,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole($role)
+    {
+        if (is_string($role)) {
+            return $this->roles->contains('name', $role);
+        }
+
+        return !! $role->intersect($this->roles)->count();
+    }
+
+    public function assignRole($role)
+    {
+        $this->roles()->sync($role, false);
+    }
+
+    public function revokeRole($role)
+    {
+        $this->roles()->detach($role);
+    }
 }
